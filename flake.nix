0dcdc284d0056c6bed3d9e3a555d9e46871921a5
Henning <>
2026-04-24 23:18:27 -0600

    Added openssl

--- flake.nix
+++ flake.nix
@@ -2,15 +2,17 @@
   description = "MA2006B Reto FJ26";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21.05"; # Last release with Python 3.6
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    nixpkgs-21-05.url = "github:NixOS/nixpkgs/21.05"; # Last release with Python 3.6
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-21-05, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python36;
+        pkgs-21-05 = import nixpkgs-21-05 { inherit system; };
+        python = pkgs-21-05.python36;
         pythonEnv = python.withPackages (ps: with ps; [
           # Backend API
           flask
@@ -33,7 +35,7 @@
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pythonEnv ];
+          buildInputs = [ pythonEnv pkgs.openssl ];
           shellHook = ''
             export PS1="[dev] \u@\h:\w$ "
           '';
